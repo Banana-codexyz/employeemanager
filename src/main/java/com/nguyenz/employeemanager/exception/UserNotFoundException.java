0dd7dcdffc8bbbf65d6090e202b5acf330f0a2a4
Nguyen Nguyen Khoi <nknguyen@IfiSolution.local>
2022-08-10 17:30:52 +0700

    add repo,service,rest

---
+++ src/main/java/com/nguyenz/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nguyenz.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
